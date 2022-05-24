abfc4d5ba260367905fbc3be9ed0c3e6feffebaf
tmenture <thomasoxemail@gmail.com>
2022-05-24 13:46:12 -0400

    added comment module, and routes to add and see comments

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+const Comment = require('./Comment');
 const Vote = require('./Vote');
 const User = require('./User');
 const Post = require('./Post');
@@ -39,4 +40,20 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
